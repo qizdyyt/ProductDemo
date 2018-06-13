f534f100cd62f58a362095732fed36bc7b9d0eef
祁子栋 <qizdyyt@163.com>
2018-06-13 09:56:24 +0800

    项目初始化

---
+++ ProductDemo/ProductDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProductDemo
+//
+//  Created by qizd on 2018/6/13.
+//  Copyright © 2018年 qizd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProductDemo/ProductDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ProductDemo
+//
+//  Created by qizd on 2018/6/13.
+//  Copyright © 2018年 qizd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
