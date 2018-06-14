e612341a7eaafde426e13c277999d3e2ef97df09
祁子栋 <qizdyyt@163.com>
2018-06-14 15:11:03 +0800

    commit code

--- ProductDemo/ProductDemo/Tools/ZDPermissions/ZDPermissionMicrophone.m
+++ ProductDemo/ProductDemo/Tools/ZDPermissions/ZDPermissionMicrophone.m
@@ -43,6 +43,64 @@
 }
 
 + (void)authorizeWithCompletion:(void (^)(BOOL, BOOL))completion {
-    
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    if ( @available(iOS 8,*) ) {
+        AVAudioSessionRecordPermission permission = [audioSession recordPermission];
+        switch (permission) {
+            case AVAudioSessionRecordPermissionGranted: {
+                if (completion) {
+                    completion(YES, NO);
+                }
+            }
+                break;
+            case AVAudioSessionRecordPermissionDenied: {
+                if (completion) {
+                    completion(NO, NO);
+                }
+            }
+                break;
+            case AVAudioSessionRecordPermissionUndetermined:
+            {
+                AVAudioSession *session = [[AVAudioSession alloc] init];
+                NSError *error;
+                [session setCategory:@"AVAudioSessionCategoryPlayAndRecord" error:&error];
+                [session requestRecordPermission:^(BOOL granted) {
+                    if (completion) {
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            completion(granted,YES);
+                        });
+                    }
+                }];
+            }
+                break;
+            default:
+            {
+                completion(NO,YES);
+            }
+                break;
+        }
+    }
+    else {
+        if([audioSession respondsToSelector:@selector(requestRecordPermission:)]) {
+            [audioSession requestRecordPermission:^(BOOL granted) {
+                
+                NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
+                
+                if (completion) {
+                    BOOL hasBeenAskedPermission = [ud boolForKey:kHasBeenAskedForMicrophonePermission];
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        completion(granted,!hasBeenAskedPermission);
+                    });
+                }
+                
+                [ud setBool:YES forKey:kHasBeenAskedForMicrophonePermission];
+                [ud synchronize];
+            }];
+        }
+        else
+        {
+            completion(YES, NO);
+        }
+    }
 }
 @end
